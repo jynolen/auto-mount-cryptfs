355c68dcb1ae845ac9be12bbfea46e1b9cdf2940
Jean-Yves NOLEN <jynolen+github@gmail.com>
2020-11-30 07:56:04 +0100

    Update mout_ssh.sh

--- mout_ssh.sh
+++ mout_ssh.sh
@@ -7,3 +7,10 @@ if [[ -d /block/cipher/$USER ]]; then
       screen -d -m ssh_mount --volume /block/cipher/$USER unlock --target $HOME/private
     fi
 fi
+
+ ➜ cat /etc/pam.d/common-session |tail -n 2
+# end of pam-auth-update config
+session optional pam_exec.so /usr/local/bin/ssh_mount --volume "" unmount
+  ➜ cat /usr/local/bin/ssh_mount
+#!/bin/sh
+/opt/ssh_mount/bin/python /opt/ssh_mount/ssh_mount.py $@
